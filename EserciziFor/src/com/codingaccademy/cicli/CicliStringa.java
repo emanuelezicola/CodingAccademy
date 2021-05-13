79a0f7790a46a551c64384c41e98fae91bda6c65
Emanuele Zicola <emanuelezicola95@gmail.com>
2021-05-13 23:45:52 +0200

    implementazione metodo che stampa occorrenze per ogni carattere della stringa

--- EserciziFor/src/com/codingaccademy/cicli/CicliStringa.java
+++ EserciziFor/src/com/codingaccademy/cicli/CicliStringa.java
@@ -1,6 +1,7 @@
 package com.codingaccademy.cicli;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CicliStringa {
 
@@ -94,8 +95,8 @@ public class CicliStringa {
     }
 
     /**
-     * @param stringaInput
-     * @param substring
+     * @param stringaInput la stringa in input
+     * @param substring la sottostringa da trovare nella stringa in input
      * @return true se la sottostringa esiste nella stringa input, altrimenti false
      * @author Daniel Minghini
      * metodo che torna true se trova una sottostringa all'interno di una stringa, altrimenti false
@@ -119,7 +120,7 @@ public class CicliStringa {
     /**
      * @author Daniel Minghini
      * metodo che controlla se la stringa è palindroma(paraculata)
-     * @param stringaInput
+     * @param stringaInput la stringa in input
      * @return true se la stringa è palindroma, altrimenti false
      */
     public static boolean isPalindromaNonOttimizzata(String stringaInput) {
@@ -140,7 +141,7 @@ public class CicliStringa {
     /**
      * @author Daniel Minghini
      * metodo che controlla se una stringa è palindroma (non paraculata)
-     * @param stringaInput
+     * @param stringaInput la stringa in input
      * @return
      */
     public static boolean isPalindromaOttimizzata(String stringaInput) {
@@ -163,5 +164,37 @@ public class CicliStringa {
             indiceScende--;
         }
         return true;
+    }
+
+
+    /**
+     * @author Emanuele Zicola
+     * Metodo che stampa le occorrenze di tutti i caratteri di una stringa in ordine alfabetico
+     * @param inputString la stringa da analizzare.
+     */
+    public static void stampaOccorrenzePerOgniCarattere(String inputString) {
+        Map<Character, Integer> mappaDiRitorno = new HashMap<>();
+
+        //popolo la mappa
+        for (int i = 0; i < inputString.length(); i++) {
+            Character charItem = inputString.charAt(i);
+            //se trovo il carattere nella mappa ne aggiorno il contatore
+            if (mappaDiRitorno.containsKey(charItem)) {
+                mappaDiRitorno.put(charItem, mappaDiRitorno.get(charItem) + 1);
+            } else {
+                //se non trovo il carattere lo inserisco nella mappa con il contatore a 1
+                mappaDiRitorno.put(charItem, 1);
+            }
+        }
+
+
+        System.out.println(inputString);
+        //recupero l'insieme delle chiavi della mappa e lo ordino alfabeticamente
+        Set<Character> keySet = mappaDiRitorno.keySet().stream().sorted(Character::compareTo).collect(Collectors.toCollection(LinkedHashSet::new));
+
+        //per ogni carattere nell'insieme delle chiavi ordinato lo stampo e recupero dalla mappa dei caratteri il contatore di quante volte il carattere è stato trovato
+        for (Character charItem : keySet) {
+            System.out.println(charItem + ") " + mappaDiRitorno.get(charItem));
+        }
     }
 }
